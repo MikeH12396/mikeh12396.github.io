c73ed92afbd2c4db5513c4229308453b4c5f827b
MikeH12396 <mikephyland1@outlook.ie>
2025-11-21 22:42:50 +0000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function count(){
-	for(i=99;i>=0;i--){
-		document.getElementById("main").innerHTML+=i+" bottles of beer on the wall, "+i+" bottles of beer,<br />";
-		document.getElementById("main").innerHTML+="Take one down and pass it around, "+(i-1)+" bottles of beer on the wall,<br />";
-	}
-}
